239cb26196cf3d269b1092af9619cb31a9e722c2
Scott Schwab <scottschwab86@gmail.com>
2023-08-28 17:18:31 -0500

    Complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    # badges = []
+    # for name in names:
+    #     badges.append(badge_maker(name))
+    # return badges
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    # welcome_msgs_with_rooms = []
+    # for index, name in enumerate(names):
+    #     welcome_msgs_with_rooms.append(f"Hello, {name}! You'll be assigned to room {index + 1}!")
+    # return welcome_msgs_with_rooms
+    return [f"Hello, {name}! You'll be assigned to room {index + 1}!" for index, name in enumerate(names)]
+
 
 def printer(names):
-    return None
+    for name in names:
+        print(badge_maker(name))
+    for message in assign_rooms(names):
+        print (message)
